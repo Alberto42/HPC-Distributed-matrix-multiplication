3f55d6559671798a044c2ef7e3545284e63be22d
Albert <albert.cieslak2@gmail.com>
2019-05-17 09:47:20 +0200

    parse args

--- main.cpp
+++ main.cpp
@@ -1,6 +1,65 @@
 #include <iostream>
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include <boost/program_options.hpp>
+#include <string>
 
-int main() {
+namespace po = boost::program_options;
+using namespace std;
+
+struct ProgramSpec {
+    string file;
+    int seed,c,exponent,g;
+    bool verbose,i,m;
+
+    ProgramSpec() : g(-1), verbose(false), i(false), m(false) {}
+
+};
+void parseArgs(int argc, const char **argv, ProgramSpec &s) {
+    po::options_description desc{"Options"};
+    try {
+        desc.add_options()
+                ("f", po::value<std::string>(), "Sparse matrix file")
+                ("s", po::value<int>(), "Seed")
+                ("c", po::value<int>())
+                ("e", po::value<int>())
+                ("g", po::value<int>())
+                ("v", "verbose")
+                ("i", "i")
+                ("m", "m")
+                ("help", "help_message");
+
+        po::variables_map vm;
+        store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);
+        if (vm.count("help")) {
+            std::cout << desc << '\n';
+            exit(0);
+        }
+        s.file = vm["f"].as<string>();
+        s.seed = vm["s"].as<int>();
+        s.c = vm["c"].as<int>();
+        s.exponent = vm["e"].as<int>();
+        s.g = vm["g"].as<int>();
+        if (vm.find("v") != vm.end()) {
+            s.verbose = true;
+        }
+        if (vm.find("i") != vm.end()) {
+            s.i = true;
+        }
+        if (vm.find("m") != vm.end()) {
+            s.m = true;
+        }
+    }
+    catch (const po::error &ex) {
+        std::cerr << ex.what() << '\n';
+    }
+    catch (...) {
+        std::cerr << desc << '\n';
+    }
+}
+int main(int argc,const char **argv) {
+    ProgramSpec s;
+    parseArgs(argc, argv, s);
     std::cout << "Hello, World!" << std::endl;
-    return 0;
 }
